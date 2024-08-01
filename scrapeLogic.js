085ef2f5b5f9cbc4d21ad0040cb191001d0afac5
JusAdev <udartdesign@gmail.com>
2024-08-01 04:40:29 -0300

    v4 working

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -1,26 +1,33 @@
-const puppeteer = require("puppeteer");
-require("dotenv").config();
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+require('dotenv').config();
+
+// Use the stealth plugin
+puppeteer.use(StealthPlugin());
 
 const scrapeLogic = async (url, selector, res) => {
   const browser = await puppeteer.launch({
-    args: [
-      "--disable-setuid-sandbox",
-      "--no-sandbox",
-      //"--single-process",
-      //"--no-zygote",
-    ],
+    headless: true,
     executablePath:
-      process.env.NODE_ENV === "production"
+      process.env.NODE_ENV === 'production'
         ? process.env.PUPPETEER_EXECUTABLE_PATH
         : puppeteer.executablePath(),
   });
+
   try {
     const page = await browser.newPage();
-
+   
     // Set a default timeout of 60 seconds for all operations
     page.setDefaultTimeout(160000);
 
     await page.goto(url);
+
+    await page.mouse.move(100, 200);
+    await page.mouse.move(150, 250, { steps: 10 });
+    await page.keyboard.type('Hello World', { delay: 100 });
+
+    // Stealth plugin will handle recaptcha solving
+    // await page.solveRecaptchas();
 
     // Set screen size
     await page.setViewport({ width: 1080, height: 1024 });
@@ -31,10 +38,21 @@ const scrapeLogic = async (url, selector, res) => {
 
     // Get the text content of the element matching the selector
     const element = await page.$(selector);
-    const textContent = await element.evaluate((el) => el.textContent);
+    const textContent = await element.evaluate(el => el.textContent);
+
+
+    let disponivel;
+    if (textContent.includes('Sorry, this item is no longer available!') || pcontent.includes("Sorry, the page you requested can not be found")) {
+      disponivel = 'Produto NÃO disponível.';
+      console.log(disponivel);
+    } else {
+      disponivel = 'Produto disponível.';
+    }
+
+
 
     // Print the text content
-    const logStatement = `The text content of the selected element is: ${textContent} <br> and content = ${pcontent}`;
+    const logStatement = `The text content of the selected element is: ${textContent} <br> and DISPONÍVEL = ${disponivel} <br> and content = ${pcontent}`;
     console.log(logStatement);
     res.send(logStatement);
   } catch (e) {
