907f1cfe24b304218f7d9d72691f39cbe905f0ef
JusAdev <udartdesign@gmail.com>
2024-08-01 01:09:33 -0300

    v3 pcontent

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -18,22 +18,23 @@ const scrapeLogic = async (url, selector, res) => {
     const page = await browser.newPage();
 
     // Set a default timeout of 60 seconds for all operations
-    page.setDefaultTimeout(60000);
+    page.setDefaultTimeout(160000);
 
     await page.goto(url);
 
     // Set screen size
     await page.setViewport({ width: 1080, height: 1024 });
+    const pcontent = await page.content();
 
     // Wait for the selector to appear with a 60-second timeout
-    await page.waitForSelector(selector, { timeout: 60000 });
+    await page.waitForSelector(selector, { timeout: 160000 });
 
     // Get the text content of the element matching the selector
     const element = await page.$(selector);
     const textContent = await element.evaluate((el) => el.textContent);
 
     // Print the text content
-    const logStatement = `The text content of the selected element is: ${textContent}`;
+    const logStatement = `The text content of the selected element is: ${textContent} <br> and content = ${pcontent}`;
     console.log(logStatement);
     res.send(logStatement);
   } catch (e) {
